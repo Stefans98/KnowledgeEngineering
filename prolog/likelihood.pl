4c5b33e97a201fb1ce044a1eedd825f7f13a1acc
Stefan Beljic <stefan.beljic23@gmail.com>
2021-04-22 17:18:14 +0200

    Likelihood

---
+++ prolog/likelihood.pl
@@ -0,0 +1,31 @@
+likelihood(carryOff_GPS_Attack, low).
+likelihood(terrestrial_Jamming, low).
+likelihood(evil_Twin_WiFi_Attack).
+likelihood(cellular_Rogue_Base_Station).
+likelihood(sniffing_Network_Traffic).
+likelihood(accessing_Intercepting_Modifying_HTTP_Cookies, high).
+likelihood(utilizing_RESTs_Trust_in_the_System_Resource_to_Obtain_Sensitive_Data, medium).
+likelihood(cellular_Traffic_Intercept).
+likelihood(sniff_Application_Code, low).
+likelihood(probe_Audio_and_Video_Peripherals, low).
+likelihood(wiFi_MAC_Address_Tracking, low).
+likelihood(wiFi_SSID_Tracking).
+likelihood(cellular_Broadcast_Message_Request).
+likelihood(signal_Strength_Tracking).
+likelihood(tcp_SYN_Scan).
+likelihood(tcp_Connect_Scan).
+likelihood(tcp_FIN_Scan).
+likelihood(tcp_Xmas_Scan).
+likelihood(tcp_Null_Scan).
+likelihood(tcp_ACK_Scan).
+likelihood(tcp_Window_Scan).
+likelihood(tcp_RPC_Scan).
+likelihood(udp_Scan).
+likelihood(padding_Oracle_Crypto_Attack).
+likelihood(cryptanalysis_of_Cellular_Encryption).
+likelihood(xml_Routing_Detour_Attacks, high).
+likelihood(transaction_or_Event_Tampering_via_Application_API_Manipulation).
+likelihood(content_Spoofing_Via_Application_API_Manipulation).
+likelihood(navigation_Remapping_To_Propagate_Malicious_Content).
+likelihood(aplication_API_Button_Hijacking).
+likelihood(leveraging_Active_Man_in_the_Middle_Attacks_to_Bypass_Same_Origin_Policy).
