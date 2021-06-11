05d47c5114ca53a0e385c633035673c1480eeee6
Stefan <stefan.savic98@gmail.com>
2021-06-11 02:09:32 +0200

    Example of attacks overview implemented

--- react-information-security-system/src/components/MainLayout.js
+++ react-information-security-system/src/components/MainLayout.js
@@ -3,13 +3,55 @@ import "../assets/styles/mainLayout.css";
 import Attacks from "./Attacks";
 
 export default class MainLayout extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  showAttacks1 = () => {
+    if (
+      (document.getElementById("div1").className = "col-md-4 col-xl-3 opacity")
+    ) {
+      document.getElementById("div1").className = "col-md-4 col-xl-3";
+      document.getElementById("div2").className = "col-md-4 col-xl-3 opacity";
+      document.getElementById("div3").className = "col-md-4 col-xl-3 opacity";
+      document.getElementById("div4").className = "col-md-4 col-xl-3 opacity";
+    }
+  };
+
+  showAttacks2 = () => {
+    document.getElementById("div2").className = "col-md-4 col-xl-3";
+    document.getElementById("div1").className = "col-md-4 col-xl-3 opacity";
+    document.getElementById("div3").className = "col-md-4 col-xl-3 opacity";
+    document.getElementById("div4").className = "col-md-4 col-xl-3 opacity";
+  };
+
+  showAttacks3 = () => {
+    document.getElementById("div3").className = "col-md-4 col-xl-3";
+    document.getElementById("div1").className = "col-md-4 col-xl-3 opacity";
+    document.getElementById("div2").className = "col-md-4 col-xl-3 opacity";
+    document.getElementById("div4").className = "col-md-4 col-xl-3 opacity";
+  };
+
+  showAttacks4 = () => {
+    document.getElementById("div4").className = "col-md-4 col-xl-3";
+    document.getElementById("div1").className = "col-md-4 col-xl-3 opacity";
+    document.getElementById("div2").className = "col-md-4 col-xl-3 opacity";
+    document.getElementById("div3").className = "col-md-4 col-xl-3 opacity";
+  };
+
   render() {
     return (
       <div>
         <div class="container" style={{ marginTop: "20px" }}>
           <div class="row">
-            <div class="col-md-4 col-xl-3">
-              <div class="card bg-c-blue order-card">
+            <div
+              className="col-md-4 col-xl-3"
+              id="div1"
+              onClick={this.showAttacks1}
+              style={{ cursor: "pointer" }}
+            >
+              <div class="card bg-c-pink order-card">
                 <div class="card-block">
                   <h5 class="m-b-20">
                     <h2 class="text-right">
@@ -21,8 +63,13 @@ export default class MainLayout extends Component {
               </div>
             </div>
 
-            <div class="col-md-4 col-xl-3">
-              <div class="card bg-c-green order-card">
+            <div
+              class="col-md-4 col-xl-3 opacity"
+              id="div2"
+              onClick={this.showAttacks2}
+              style={{ cursor: "pointer" }}
+            >
+              <div class="card bg-c-blue order-card">
                 <div class="card-block">
                   <h5 class="m-b-20">
                     <h2 class="text-right">
@@ -34,7 +81,12 @@ export default class MainLayout extends Component {
               </div>
             </div>
 
-            <div class="col-md-4 col-xl-3">
+            <div
+              class="col-md-4 col-xl-3 opacity"
+              id="div3"
+              onClick={this.showAttacks3}
+              style={{ cursor: "pointer" }}
+            >
               <div class="card bg-c-yellow order-card">
                 <div class="card-block">
                   <h5 class="m-b-20">
@@ -47,8 +99,13 @@ export default class MainLayout extends Component {
               </div>
             </div>
 
-            <div class="col-md-4 col-xl-3">
-              <div class="card bg-c-pink order-card">
+            <div
+              class="col-md-4 col-xl-3 opacity"
+              id="div4"
+              onClick={this.showAttacks4}
+              style={{ cursor: "pointer" }}
+            >
+              <div class="card bg-c-green order-card">
                 <div class="card-block">
                   <h5 class="m-b-20">
                     <h2 class="text-right">
