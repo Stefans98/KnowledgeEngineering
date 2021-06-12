0be3635a9765d432b5b3046b1af0337395703f8f
aleksaivanic <ivanicaleksa98@gmail.com>
2021-06-12 22:46:37 +0200

    Cbr frontend implemented

--- react-information-security-system/src/components/CaseBaseReasoning.js
+++ react-information-security-system/src/components/CaseBaseReasoning.js
@@ -1,7 +1,218 @@
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 import React, { Component } from "react";
+import {
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Grid,
+  Radio,
+  RadioGroup,
+} from "@material-ui/core";
 
 export default class CaseBaseReasoning extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputPrerequisitesValue: [],
+      inputConsequencesValue: [],
+      inputWeaknessesValue: [],
+      inputMitigationsValue: [],
+      likelihood: "0",
+      severity: "0"
+    };
+  }
+
+  handleChangeLikelihood = (event) => {
+    this.setState({ likelihood: event.target.value });
+  };
+
+  handleChangeSeverity = (event) => {
+    this.setState({ severity: event.target.value });
+  };
+
+  propagate = () => {
+    console.log(this.state.inputPrerequisitesValue);
+    console.log(this.state.inputConsequencesValue);
+    console.log(this.state.inputWeaknessesValue);
+    console.log(this.state.inputMitigationsValue);
+    console.log(this.state.likelihood);
+    console.log(this.state.severity);
+  }
+
   render() {
-    return <div>CaseBaseReasoning</div>;
+    const continets = [
+      { state: "Europe" },
+      { state: "Asia" },
+      { state: "North America" },
+      { state: "South America" },
+      { state: "Australia" },
+    ];
+
+    const industry = [
+      { state: "Services" },
+      { state: "Manufacturing" },
+      { state: "Finance, Insurance & Real Estate" },
+      { state: "Public Administration" },
+      { state: "Nonclassifiable Establishments" },
+    ];
+
+    const companySize = [
+      { state: "1-250" },
+      { state: "251-500" },
+      { state: "501-1000" },
+      { state: "1000+" },
+    ];
+    return (
+      <div>
+         <div
+          class="bg-white rounded shadow-sm p-4 mb-5 rating-review-select-page"
+          style={{ maxWidth: "100%" }}
+        >
+          <h4 style={{ textAlign: "left", color: "#74767a" }}>
+            Choose attack characteristics:
+          </h4>
+
+          <Grid container spacing={5}>
+            <Grid item xs>
+              <Autocomplete
+                multiple
+                disableCloseOnSelect
+                id="prerequisites"
+                onChange={(event, newInputValue) => {
+                  this.setState({ inputPrerequisitesValue: newInputValue }, () => {
+                    console.log(this.state.inputPrerequisitesValue);
+                  });
+                }}
+                options={continets}
+                getOptionLabel={(option) => option.state}
+                style={{ width: 600 }}
+                renderInput={(params) => (
+                  <TextField {...params} label="Prerequisites" variant="outlined" />
+                )}
+              />
+            </Grid>
+            <Grid item xs>
+              <Autocomplete
+                multiple
+                disableCloseOnSelect
+                id="consequences"
+                onChange={(event, newInputValue) => {
+                  this.setState({ inputConsequencesValue: newInputValue }, () => {
+                    console.log(this.state.inputConsequencesValue);
+                  });
+                }}
+                options={industry}
+                getOptionLabel={(option) => option.state}
+                style={{ width: 600 }}
+                renderInput={(params) => (
+                  <TextField {...params} label="Consequences" variant="outlined" />
+                )}
+              />
+            </Grid>
+            <Grid item xs>
+              <Autocomplete
+                multiple
+                disableCloseOnSelect
+                id="weaknesses"
+                onChange={(event, newInputValue) => {
+                  this.setState(
+                    { inputWeaknessesValue: newInputValue },
+                    () => {
+                      console.log(this.state.inputWeaknessesValue);
+                    }
+                  );
+                }}
+                options={companySize}
+                getOptionLabel={(option) => option.state}
+                style={{ width: 600 }}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Weaknesses"
+                    variant="outlined"
+                  />
+                )}
+              />
+            </Grid>
+            <Grid item xs>
+              <Autocomplete
+                multiple
+                disableCloseOnSelect
+                id="mitigations"
+                onChange={(event, newInputValue) => {
+                  this.setState(
+                    { inputMitigationsValue: newInputValue },
+                    () => {
+                      console.log(this.state.inputMitigationsValue);
+                    }
+                  );
+                }}
+                options={companySize}
+                getOptionLabel={(option) => option.state}
+                style={{ width: 600 }}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    label="Mitigations"
+                    variant="outlined"
+                  />
+                )}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid container spacing={5}>
+            <Grid item xs>
+              <FormControl component="fieldset">
+                <FormLabel component="legend">
+                  Likelihood
+                </FormLabel>
+                <RadioGroup
+                  aria-label="interactingWithSystem"
+                  name="interactingWithSystem"
+                  value={this.state.likelihood}
+                  onChange={this.handleChangeLikelihood}
+                >
+                  <FormControlLabel value="0" control={<Radio />} label="Low" />
+                  <FormControlLabel value="1" control={<Radio />} label="Medium" />
+                  <FormControlLabel value="2" control={<Radio />} label="High" />
+                </RadioGroup>
+              </FormControl>
+            </Grid>
+            
+            <Grid item xs>
+              <FormControl component="fieldset">
+                <FormLabel component="legend">
+                  Severity
+                </FormLabel>
+                <RadioGroup
+                  aria-label="interactingWithSystem"
+                  name="interactingWithSystem"
+                  value={this.state.severity}
+                  onChange={this.handleChangeSeverity}
+                >
+                  <FormControlLabel value="0" control={<Radio />} label="Low" />
+                  <FormControlLabel value="1" control={<Radio />} label="Medium" />
+                  <FormControlLabel value="2" control={<Radio />} label="High" />
+                </RadioGroup>
+              </FormControl>
+            </Grid>          
+          </Grid>
+
+          <div class="graph-star-rating-footer text-center mt-4 mb-3">
+            <button
+              type="button"
+              onClick={this.propagate}
+              class="btn btn-outline-primary btn-sm"
+              style={{ width: "200px", fontSize: "17px" }}
+            >
+              Propagate
+            </button>
+          </div>
+
+        </div>
+      </div>
+    );
   }
 }
--- spring-boot-information-security-system/src/main/java/knowledge/engineering/information/security/system/controller/CbrController.java
+++ spring-boot-information-security-system/src/main/java/knowledge/engineering/information/security/system/controller/CbrController.java
@@ -2,15 +2,19 @@ package knowledge.engineering.information.security.system.controller;
 
 import knowledge.engineering.information.security.system.cbr.CbrApplication;
 import knowledge.engineering.information.security.system.dto.CbrDto;
-import knowledge.engineering.information.security.system.model.Level;
+import knowledge.engineering.information.security.system.dto.CbrParamsDto;
+import knowledge.engineering.information.security.system.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ucm.gaia.jcolibri.cbrcore.CBRCase;
+import ucm.gaia.jcolibri.util.FileIO;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,10 +24,75 @@ public class CbrController {
     @Autowired
     private CbrApplication cbrApplication;
 
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<CbrDto>> calculateVulnerabilityRisk(@RequestBody CbrParameters cbrParameters) {
+
+        List<CbrDto> result = cbrApplication.cbrResult(Level.HIGH, Level.MEDIUM, cbrParameters.getPrerequisites(),
+                cbrParameters.getConsequences(), cbrParameters.getWeaknesses(), cbrParameters.getMitigations());
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<CbrDto>> getSimilarity() {
         List<CbrDto> result = cbrApplication.cbrResult(Level.HIGH, Level.MEDIUM, "cookie_is_contained_in_reply_to_adversary",
                 "gain_privileges", "missing_Encryption_of_Sensitive_Data", "unspecified");
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
+
+    @GetMapping(value="getCbrParams", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CbrParamsDto> getAllCbrParameters() {
+        CbrParamsDto cbrParamsDto = new CbrParamsDto();
+
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(FileIO.openFile("./src/main/resources/data/attacks.csv")));
+            if (br == null)
+                throw new Exception("Error opening file");
+
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("#") || (line.length() == 0))
+                    continue;
+                String[] values = line.split(";");
+
+                // Prerequisites
+                String[] prerequisitesStrings = values[4].split(",");
+                List<String> prerequisitesList = new ArrayList<>();
+                for(int i = 0; i < prerequisitesStrings.length; i++){
+                    prerequisitesList.add(prerequisitesStrings[i]);
+                }
+                cbrParamsDto.setPrerequisites(prerequisitesList);
+                Prerequisites prerequisites = new Prerequisites();
+                prerequisites.setName(values[4]);
+
+                Consequences consequences = new Consequences();
+                if(!values[5].equals("unspecified")) {
+                    consequences.setName(values[5]);
+                } else {
+                    consequences.setName("");
+                }
+
+                Weaknesses weaknesses = new Weaknesses();
+                if(!values[6].equals("unspecified")) {
+                    weaknesses.setName(values[6]);
+                } else {
+                    weaknesses.setName("");
+                }
+
+                Mitigations mitigations = new Mitigations();
+                if(!values[7].equals("unspecified")) {
+                    mitigations.setName(values[7]);
+                } else {
+                    mitigations.setName("");
+                }
+            }
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<>(cbrParamsDto, HttpStatus.OK);
+    }
+
 }
